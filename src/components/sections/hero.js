9cf29501a4db8e0c3b84c5eeaef875e784bace15
Sri Bhargav <70511544+SriBhargav8@users.noreply.github.com>
2022-03-13 12:11:37 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,12 +59,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Siddappa.</h2>;
+  const three = <h3 className="big-heading">I build things which i dont know.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+        I’m a computer science engineer specializing in Data Science (and occasionally do naughty things) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
